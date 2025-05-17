c7592bfa2866644f79c02d9acfe9c2b01187b1fe
Hermann <188955756+4483-s@users.noreply.github.com>
2025-05-17 12:40:24 +0800

    rewrote functions

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const myLibrary = [];
 const table = document.querySelector(".list");
 const addBtn = document.querySelector(".add");
+addBtn.addEventListener("click", function () {
+  addBookToLibrary(
+    document.querySelector("#title").value,
+    document.querySelector("#author").value,
+    document.querySelector("#pages").value,
+    document.querySelector("#read").value
+  );
+  console.log(myLibrary);
+  displayBooks();
+});
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -16,21 +26,57 @@ Book.prototype.info = function () {
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
+function displayNumbers() {
+  const number = document.querySelectorAll(".number");
+  number.forEach((v, i) => {
+    v.textContent = i + 1;
+  });
+}
 function displayBooks() {
+  while (table.firstChild) {
+    table.removeChild(table.firstChild);
+  }
   myLibrary.forEach((v, i) => {
     const aBook = document.createElement("tr");
     const nCell = document.createElement("td");
-    const titleCell = document.createElement("tr");
-    const authorCell = document.createElement("tr");
-    const pagesCell = document.createElement("tr");
-    const readCell = document.createElement("tr");
-    aBook.append(nCell, titleCell, authorCell, pagesCell, readCell);
-    nCell.textContent = i + 1;
+    const titleCell = document.createElement("td");
+    const authorCell = document.createElement("td");
+    const pagesCell = document.createElement("td");
+    const readCell = document.createElement("td");
+    const idCell = document.createElement("td");
+    const rmBtnCell = document.createElement("td");
 
+    const rmBtn = document.createElement("button");
+    idCell.classList.add("id");
+    nCell.classList.add("number");
     titleCell.textContent = v.title;
-    authorCell.textContent = v.title;
-    pagesCell.textContent = v.title;
-    readCell.textContent = v.title;
-    titleCell.textContent = v.title;
+    authorCell.textContent = v.author;
+    pagesCell.textContent = v.pages;
+    readCell.textContent = v.read;
+    idCell.textContent = v.id;
+    rmBtn.textContent = "Remove";
+    rmBtn.addEventListener("click", function () {
+      myLibrary.splice(
+        myLibrary.findIndex((v) => (v.id = idCell.textContent)),
+        1
+      );
+      aBook.remove();
+
+      displayNumbers();
+    });
+
+    rmBtnCell.append(rmBtn);
+    aBook.append(
+      nCell,
+      titleCell,
+      authorCell,
+      pagesCell,
+      readCell,
+      idCell,
+      rmBtnCell
+    );
+
+    table.append(aBook);
   });
+  displayNumbers();
 }
